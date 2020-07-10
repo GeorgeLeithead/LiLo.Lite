b73b9ab49589c4e67491522965686443d64d9cad
github@internetwideworld.com <github@internetwideworld.com>
2020-07-10 12:59:55 +0100

    - Fixed an issue with AppCenter configuration - Added basic event tracking for selected market currency

--- Source/LiLo.Lite/LiLo.Lite/Views/ChartView.xaml.cs
+++ Source/LiLo.Lite/LiLo.Lite/Views/ChartView.xaml.cs
@@ -13,15 +13,15 @@
 
 namespace LiLo.Lite.Views
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using Lilo.Lite;
+	using Microsoft.AppCenter.Analytics;
+	using PanCardView;
 	using Xamarin.Forms;
 	using Xamarin.Forms.Internals;
 	using Xamarin.Forms.Xaml;
-	using PanCardView;
-	using Xamarin.Forms.Markup;
-	using Lilo.Lite;
-	using System;
-	using System.Globalization;
-	using System.Collections;
 
 	/// <summary>Chart View class.</summary>
 	[Preserve(AllMembers = true)]
@@ -41,34 +41,36 @@ namespace LiLo.Lite.Views
 			return true; // prevent users from clicking the back button and exiting the application from the root page.
 		}
 
-		/// <summary>Navigating in the Web view</summary>
-		/// <param name="sender">Sender object</param>
-		/// <param name="e">Web navigation Event arguments.</param>
-		/// <remarks>Prevent users from navigating to anywhere else other than where we want.</remarks>
-		private void WebView_Navigating(object sender, WebNavigatingEventArgs e) => e.Cancel = true;
-
-		/// <summary>Transition effect when the cover flow view item has appeared.</summary>
-		/// <param name="view">Cards view object</param>
-		/// <param name="args">Item appeared event arguments.</param>
-		private async void OnCoverFlowViewItemAppeared(CardsView view, PanCardView.EventArgs.ItemAppearedEventArgs args)
-		{
-//			if (view.SelectedIndex != 0)
-//			{
-				Frame frame = view.CurrentView as Frame;
-				StackLayout stackLayoutFrame = frame.Children[0] as StackLayout;
-				await stackLayoutFrame.Children[0].ScaleTo(1.1, 200, Easing.CubicInOut);
-				await stackLayoutFrame.Children[0].ScaleTo(1, 500, Easing.CubicIn);
-				StackLayout stackLayoutContainer = frame.Parent.Parent as StackLayout;
-				WebView webView = stackLayoutContainer.Children[stackLayoutContainer.Children.Count - 1] as WebView;
-				webView.Source = WebViewSource(frame.AutomationId);
-//			}
-		}
-
 		private static HtmlWebViewSource WebViewSource(string symbol)
 		{
 			string colorTheme = Application.Current.UserAppTheme == OSAppTheme.Dark ? "dark" : "light";
 			HtmlWebViewSource htmlViewSource = new HtmlWebViewSource() { Html = GlobalSettings.TradingViewWebViewSource.Html.Replace("XX0XX", symbol).Replace("XX1XX", TimeZoneInfo.Local.ToString()).Replace("XX2XX", colorTheme).Replace("XX3XX", CultureInfo.CurrentCulture.IetfLanguageTag.Substring(0, 2)) };
 			return htmlViewSource;
 		}
+
+		/// <summary>Transition effect when the cover flow view item has appeared.</summary>
+		/// <param name="view">Cards view object</param>
+		/// <param name="args">Item appeared event arguments.</param>
+		private async void OnCoverFlowViewItemAppeared(CardsView view, PanCardView.EventArgs.ItemAppearedEventArgs args)
+		{
+			Frame frame = view.CurrentView as Frame;
+			StackLayout stackLayoutFrame = frame.Children[0] as StackLayout;
+			await stackLayoutFrame.Children[0].ScaleTo(1.1, 200, Easing.CubicInOut);
+			await stackLayoutFrame.Children[0].ScaleTo(1, 500, Easing.CubicIn);
+			StackLayout stackLayoutContainer = frame.Parent.Parent as StackLayout;
+			WebView webView = stackLayoutContainer.Children[stackLayoutContainer.Children.Count - 1] as WebView;
+			webView.Source = WebViewSource(frame.AutomationId);
+			Dictionary<string, string> selectedCurrencyAnalytic = new Dictionary<string, string>
+			{
+				{ "Currency", frame.AutomationId }
+			};
+			Analytics.TrackEvent("Market", selectedCurrencyAnalytic);
+		}
+
+		/// <summary>Navigating in the Web view</summary>
+		/// <param name="sender">Sender object</param>
+		/// <param name="e">Web navigation Event arguments.</param>
+		/// <remarks>Prevent users from navigating to anywhere else other than where we want.</remarks>
+		private void WebView_Navigating(object sender, WebNavigatingEventArgs e) => e.Cancel = true;
 	}
 }
