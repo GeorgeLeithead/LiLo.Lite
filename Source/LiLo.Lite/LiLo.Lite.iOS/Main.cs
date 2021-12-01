b95405946136851b20dafd430e7cc786b20549cb
github@internetwideworld.com <github@internetwideworld.com>
2021-12-01 16:36:51 +0000

    Upgraded to Android 12

--- Source/LiLo.Lite/LiLo.Lite.iOS/Main.cs
+++ Source/LiLo.Lite/LiLo.Lite.iOS/Main.cs
@@ -20,6 +20,7 @@ namespace LiLo.Lite.iOS
 		/// <summary>This is the main entry point of the application.</summary>
 		/// <param name="args">Start-up arguments.</param>
 		/// <remarks>If you want to use a different Application Delegate class from "AppDelegate" you can specify it here.</remarks>
+		[System.Obsolete]
 		private static void Main(string[] args)
 		{
 			UIApplication.Main(args, null, "AppDelegate");
